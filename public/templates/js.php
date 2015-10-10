9ad85fae1b32fa5d358b638c2ac59caa07aa0ac1
Nyaundi Brian <ndieksman@gmail.com>
2015-10-10 14:15:46 +0300

    rename assets dir to lisbs

--- public/templates/js.php
+++ public/templates/js.php
@@ -10,4 +10,4 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <!-- Custom Theme JavaScript -->
-<script src="<?php echo $dir_path;?>assets/javascripts/index.js"></script>
+<script src="<?php echo $dir_path;?>libs/javascripts/index.js"></script>
