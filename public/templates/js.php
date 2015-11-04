3fe8f55ead4ee3ef10c6d92a88f450558efa7ce4
Nyaundi Brian <ndieksman@gmail.com>
2015-11-04 15:46:04 +0300

    update search input field placeholder on category select change

--- public/templates/js.php
+++ public/templates/js.php
@@ -11,3 +11,10 @@
 <script src="<?php echo $dir_path;?>assets/javascripts/bootstrap.min.js"></script>
 <!-- Custom Theme JavaScript -->
 <script src="<?php echo $dir_path;?>assets/javascripts/index.js"></script>
+ <script type="text/javascript">
+      $('#search-select')[0].onchange=function(t){
+	      t.bubbles=false;
+          s=t;
+          $('#search-input')[0].setAttribute('placeholder','Search '+t.target.selectedOptions[0].text);
+      };
+</script>
