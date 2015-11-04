d145360035a205027eed6a722a7932f12423ce7d
Nyaundi Brian <ndieksman@gmail.com>
2015-11-04 16:48:05 +0300

    reduce ad properties displayed to name,price,img and status

--- public/templates/add.php
+++ public/templates/add.php
@@ -7,45 +7,22 @@
 
 
 ?>
-<div class="col-md-4 well col-sm-12">
-	<div class="col-md-5 col-sm-12">
+<div class="col-md-2 well add-item col-sm-12">
+	<div class="col-md-12 col-sm-12">
 
 		<div class="row">
 		<img src="<?php echo 'images/'.$pic_url.'.jpg';?>" class="add_img">
 
 			<div class="col-md-12">
-				<span class="col-md-6 pull-left"><?php echo $add['name'];?></span>
-				<span class="col-md-6 text-left"><?php echo $add['color'];?></span>
-			</div>
-
+				<span class=""><?php echo $add['name'];?></span>
+			</div>		
 			<div class="col-md-12">
-				<!--Contact-->
-				<span class="col-md-12 text-left"><?php echo $add['contact'];?></span>
+				<span class=""><?php echo format_price($add['price']);?> Ksh</span>
 			</div>
-		</div>
-	</div>
-	<div class="col-md-7 col-sm-12">
-
-
-		<div class="row">
-			<div class="col-md-12">
-			<span class="col-md-12 text-center"><?php echo $add['reg'];?></span>
-			</div>
-
-			<div class="col-md-12">
-			<span class="col-md-5">Price</span><span class="col-md-7"><?php echo format_price($add['price']);?> Ksh</span>
-			</div>
-			<div class="col-md-12">
-			<span class="col-md-5">Age</span><span class="col-md-7"><?php echo $add['age'];?> Yrs</span>
-			</div>
-			<div class="col-md-12">
-			<span class="col-md-5">Location</span><span class="col-md-7"><?php echo $add['location'];?></span>
-			</div>
-			<div class="col-md-12 text-center"><p><?php echo $add['status'];?></p></div>
+			<div class="col-md-12"><p><?php echo $add['status'];?></p></div>
+			<button class="pull-right"><i class="glyphicon glyphicon-plus-sign"></i>Cart</button>
 
 		</div>
-
-
 
 	</div>
 </div>
