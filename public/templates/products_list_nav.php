8973656a9e4bfac8573ba49935d5f00d0dc35dba
Nyaundi Brian <ndieksman@gmail.com>
2015-11-04 15:53:44 +0300

    create nav for advertisement categories
    remove search form

--- public/templates/products_list_nav.php
+++ public/templates/products_list_nav.php
@@ -1,39 +1,27 @@
 <?php
+
 ?>
-<nav class="navbar navbar-default main">
+<nav class="navbar main">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+          <?php if (!$categories) {?>
+           <li ><a class="page-scroll" href="<?php echo 'admin';?>">First Add products you sell. </a></li>
+          <?php }else {?>
+             <?php foreach ($categories as $ctg) {?>
 
+              <li class="<?php echo ($_GET['category']==$ctg[0])?'active':'';?>">
+                <a class="page-scroll" href="<?php echo '?category='.$ctg[0];?>">
+                    <?php echo $ctg[1];?> <span><?php echo $ctg[2];?></span>
+                </a>
+              </li>
+             <?php } ?>
+
+          <?php }?>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <form class="navbar-form navbar-left">
-
-      <select class="form-control" name="user" id="user">
-<option value="0">Sort By</option>
-<option value="0" selected="selected">Name</option>
-<option value="0">Price</option>
-<option value="0">Age</option>
-<option value="0">Location</option>
-
-</select>
-      </form>
-      <form class="navbar-form navbar-left">
-
-      <select class="form-control" name="user" id="user">
-<option value="0" >Ascending</option>
-<option value="0" selected="selected">Descending</option>
-</select>
-      </form>
-
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
