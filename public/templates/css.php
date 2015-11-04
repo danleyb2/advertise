b9c0c0f5b7ab2c4ae4013e7271a1534445a20afb
Nyaundi Brian <ndieksman@gmail.com>
2015-11-04 16:00:07 +0300

    change body,footer,header background-color li.active background is white add add-item height and margins

--- public/templates/css.php
+++ public/templates/css.php
@@ -8,12 +8,25 @@
 <link href="<?php echo $dir_path;?>assets/stylesheets/bootstrap.min.css" rel="stylesheet">
 
 <style>
+body{
+	background-color: rgb(228, 228, 238);
+}
+footer,.navbar{
+	background-color: rgb(193, 190, 233);
+}
 .main{
     margin-top:60px;
+}
+li.active{
+	background-color: white;
 }
 .add_img{
 	width:150px;
 	height:100px;
 }
-
+.add-item{
+	height: 240px;
+	margin-left: 2px;
+	margin-right: 2px;
+}
 </style>
