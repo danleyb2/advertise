8fda8971cd735daa26ca54dc9ac0c72ee397a452
Nyaundi Brian <ndieksman@gmail.com>
2015-11-04 15:57:00 +0300

    create search form remove navbar-default class increase logo size

--- public/templates/navigation.php
+++ public/templates/navigation.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
@@ -11,9 +11,24 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="<?php echo $dir_path?>">Moto <span style="font-size: 10px;color:blue;">Beta</span></a>
+                <a class="navbar-brand page-scroll" style="font-size: 32px;" href="<?php echo $dir_path?>">Moto <span style="font-size: 10px;color:red;">Beta</span></a>
             </div>
 
+
+          <form class="navbar-form navbar-left" role="search">
+
+            <div class="form-group">
+                <select class="form-control" id="search-select">
+
+                    <option value="0" selected="selected">Search All</option>
+                    <?php foreach ($categories as $ctg) {?>
+                    <option value="<?php echo $ctg[0];?>"><?php echo $ctg[1];?></option>
+                    <? }?>
+                </select>
+                <input type="text" class="form-control" id="search-input" placeholder="Search All">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
