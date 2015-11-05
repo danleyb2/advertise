6d2c4ccf612e6f1965ebb6648287ef183eabe1b1
Nyaundi Brian <ndieksman@gmail.com>
2015-11-05 20:30:22 +0300

    create more and less categories btns functionality optimize previous js

--- public/templates/js.php
+++ public/templates/js.php
@@ -14,7 +14,51 @@
  <script type="text/javascript">
       $('#search-select')[0].onchange=function(t){
 	      t.bubbles=false;
-          s=t;
           $('#search-input')[0].setAttribute('placeholder','Search '+t.target.selectedOptions[0].text);
       };
+
+      function moreAvailable(){
+            el=$('#bs-example-navbar-collapse-2 ul')[0];
+            return(el.offsetHeight<el.scrollHeight ||
+                    el.offsetWidth<el.scrollWidth)
+        }
+
+        $('#more-cat').click(function (evt) {
+            evt.preventDefault();
+            el=$('#bs-example-navbar-collapse-2 ul')[0];
+            if(moreAvailable()){
+                $('#bs-example-navbar-collapse-2 ul').animate({'height':'+=51px'}, function () {
+                    if(!moreAvailable()) {
+                    $('#more-cat').html('Close <i class="glyphicon glyphicon-remove-circle "></i>');
+                }
+                });
+                $('#less-cat').html('Less <i class="glyphicon glyphicon-arrow-up "></i>');
+            }else{
+                $('#bs-example-navbar-collapse-2 ul').animate({'height':'50px'}, function () {
+                    $('#more-cat').html('More <i class="glyphicon glyphicon-arrow-down "></i>');
+                    $('#less-cat').html('All <i class="glyphicon glyphicon-circle-arrow-down"></i>');
+                });
+            }
+
+        });
+        $('#less-cat').click(function (evt) {
+            evt.preventDefault();
+            el=$('#bs-example-navbar-collapse-2 ul')[0];
+            if(el.offsetHeight<=50){
+                $('#bs-example-navbar-collapse-2 ul').animate({'height': el.scrollHeight+'px'}, function () {
+                    $('#more-cat').html('Close <i class="glyphicon glyphicon-remove-circle "></i>');
+                    $('#less-cat').html('Less <i class="glyphicon glyphicon-arrow-up"></i>');
+
+                });
+
+            }else{
+                $('#bs-example-navbar-collapse-2 ul').animate({'height': '-=51px'}, function () {
+                    $('#more-cat').html('More <i class="glyphicon glyphicon-arrow-down "></i>');
+                    if(el.offsetHeight<=50){
+                        $('#less-cat').html('All <i class="glyphicon glyphicon-circle-arrow-down"></i>');
+                    }
+                });
+            }
+
+        });
 </script>
