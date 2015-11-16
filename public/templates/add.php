b14802a754c8b48c1f7b0b540ca4d4156188c41e
Nyaundi Brian <ndieksman@gmail.com>
2015-11-17 01:21:38 +0300

    remove kenyan currency postfix from price

--- public/templates/add.php
+++ public/templates/add.php
@@ -18,7 +18,7 @@
 				<button class="pull-right btn btn-default">Add <i class="glyphicon glyphicon-shopping-cart"></i></button>
 			</div>
 			<div class="ad-row">
-				<span class="ad-price"><?php echo format_price($add['price']);?> Ksh</span>
+				<span class="ad-price"><?php echo format_price($add['price']);?> </span>
 			</div>
 			<div class="ad-row">
 			 <p class="ad-desc"><?php echo $add['status'];?></p>
